8b316b5b7f23150d761aaf8ffb739f7d75eded1f
mel-ferreira <melissamouraf@gmail.com>
2025-11-14 21:52:32 -0300

    [FEATURE] Alterando os testes

--- test_app.py
+++ test_app.py
@@ -2,29 +2,29 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para adicionar o atributo '__version__' em werkzeug 
 if not hasattr(werkzeug, '__version__'): 
     werkzeug.__version__ = "mock-version"
 
-class APITestCase(unittest.TestCase): 
-    @classmethod 
-    def setUpClass(cls): 
-        # Criação do cliente de teste 
-        cls.client = app.test_client()
-    
-    def test_home(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
 
-    def test_login(self):
+class APITestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):        cls.client = app.test_client()
+
+    def status_inicio_teste_400(self):
+        # Se a home responde com 400
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 400)
+
+    def itens_teste_200(self):
+        # Se items responde com status 200
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+
+    def login_teste_200(self):
+        # Se login retorna um JSON
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
-
-    def test_protected_no_token(self):
-        response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+        self.assertTrue(response.is_json)
 
 if __name__ == '__main__':
     unittest.main()
