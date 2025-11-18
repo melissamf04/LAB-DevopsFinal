5d0741bfd21251da693704bf26e5895938b8d102
mel-ferreira <melissamouraf@gmail.com>
2025-11-17 21:11:03 -0300

    [FEATURE] Criando arquivo pipeline/jobs

--- test_app.py
+++ test_app.py
@@ -10,17 +10,17 @@ class APITestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):        cls.client = app.test_client()
 
-    def status_inicio_teste_400(self):
+    def test_status_inicio_400(self):
         # Se a home responde com 400
         response = self.client.get('/')
         self.assertEqual(response.status_code, 400)
 
-    def itens_teste_200(self):
+    def test_itens_200(self):
         # Se items responde com status 200
         response = self.client.get('/items')
         self.assertEqual(response.status_code, 200)
 
-    def login_teste_200(self):
+    def test_login_200(self):
         # Se login retorna um JSON
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
